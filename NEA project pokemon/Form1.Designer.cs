d5c10445a8a6268232baf400e3a65f6452912299
Nxsh0-0 <nashitmd458@gmail.com>
2025-02-08 01:28:11 +0000

    movement with collision

--- NEA project pokemon/Form1.Designer.cs
+++ NEA project pokemon/Form1.Designer.cs
@@ -34,23 +34,27 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(86, 300);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(66, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(1523, 712);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::NEA_project_pokemon.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(1327, 643);
+            this.ClientSize = new System.Drawing.Size(1769, 791);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- NEA project pokemon/Form1.cs
+++ NEA project pokemon/Form1.cs
@@ -24,6 +24,9 @@ namespace NEA_project_pokemon
         Image man = F1;
         static int x = 25;
         static int y = 25;
+        static int manw = 30;
+        static int manh = 50;
+        static obstacle[] obstacles = new obstacle[5];
 
         public Form1()
         {
@@ -32,14 +35,93 @@ namespace NEA_project_pokemon
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
-
+        struct obstacle
+        {
+            public Image imagename;
+            public int xloc;
+            public int yloc;
+            public int width;
+            public int height;
+            public Rectangle bounds;
+        }
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawImage(man, x, y, 30, 50);
-          
             
+            e.Graphics.DrawImage(man, x, y, manw, manh);
+            
+            for (int i = 0; i < obstacles.Length; i++)
+            {
+                obstacles[i] = new obstacle
+                {
+                    imagename = tree,
+                    xloc = 60 * i,
+                    yloc = 250,
+                    width = 60,
+                    height = 100,
+                    bounds = new Rectangle(60 * i, 250, 60, 100)
+                };
+                e.Graphics.DrawImage(obstacles[i].imagename, obstacles[i].xloc, obstacles[i].yloc, obstacles[i].width, obstacles[i].height);
+            }
+
+
+        }
+        private Boolean checkCollision()
+        {
+            Boolean collision = false;
+            for (int i = 0; i < obstacles.Length; i++)
+            {
+                if (x+manw >= obstacles[i].xloc &&
+                    x <= obstacles[i].xloc + obstacles[i].width &&
+                    y+manh >= obstacles[i].yloc &&
+                    y <= obstacles[i].yloc + obstacles[i].height)
+                { collision = true; }
+            }
+            return collision;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                if (man == B1)
+                { man = B2;}
+                else man = B1;
+                if (checkCollision())
+                { y = y + 10; }
+                else y = y - 10;
+            }
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                if (man == L1)
+                { man = L2; }
+                else man = L1;
+                if (checkCollision())
+                { x = x + 10; }
+                else x = x - 10;
+            }
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                if (man == F1)
+                { man = F2; }
+                else man = F1;
+                if (checkCollision())
+                { y = y - 10; }
+                else y = y + 10;
+            }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                if (man == R1)
+                { man = R2; }
+                else man = R1;
+                if (checkCollision())
+                { x = x - 10; }
+                else x = x + 10;
+            }
+
+            pictureBox1.Refresh();
+            pictureBox1.Update();
         }
     }
 }
